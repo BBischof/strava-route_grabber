6050436e6b420e2cd506753a461f1b454f7b0083
bbischof <bryan.bischof@gmail.com>
2017-04-03 12:21:28 -0400

    Add testing example of mocked function for route with monkeypatch

--- route_grabber.py
+++ route_grabber.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+@app.route("/get_routes")
+def strava_routes_encoding():
+    response = requests.get("https://www.strava.com/routes")
+    return response.headers["Content-Encoding"]
+
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0')
